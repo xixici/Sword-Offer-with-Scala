819178e2690f8ac9ae3e82fdfb13b73359c5a2aa
xixici <202388990@qq.com>
2017-08-27 18:55:49 +0800

    Add files via upload

---
+++ Q08_跳台阶.java
@@ -0,0 +1,22 @@
+package Offer;
+
+public class Q08_ÌøÌ¨½× {
+	public int JumpFloor(int target) {
+		int a = 1, b = 1, v = 0;
+		if (target == 1) {
+			v = 1;
+		}
+		if (target == 2) {
+			v = 2;
+		}
+		if (target>=3) {
+			for(int i=3; i<=target; i++) {
+				v = a + b;
+				a = b;
+				b = v;
+			}
+		}
+		return v;
+    }
+
+}
---
+++ Q18_二叉树镜像.java
@@ -0,0 +1,5 @@
+package Offer;
+
+public class Q18_¶ş²æÊ÷¾µÏñ {
+
+}
