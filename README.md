792326b47cf6ce80474b0ef188b9834e08ccdd16
xixici <202388990@qq.com>
2019-03-14 23:58:11 +0800

    2019-03-14 23:58:10

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
 # 记录
 - Java版剑指OFFER 
-  - [x] 1-20题
+
+  - [x] 01-20题
+
 - Scala 版本剑指Offer
-  - [ ] 66题
+
+  - [x] 01-20题 2019-03-14
+
+  - [ ] 20-66题
+
 - [牛客网题目](https://www.nowcoder.com/ta/coding-interviews)
 
---
+++ src/main/scala/com/xixici/P21.scala
@@ -0,0 +1,11 @@
+package com.xixici
+
+/**
+  * Created by xixici
+  * Date: 2019/3/14 
+  * Project Name: sword-offer-scala-sbt
+  * Project URL: https://github.com/xixici/sword-offer-scala
+  **/
+object P21 {
+
+}
