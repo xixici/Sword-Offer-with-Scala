a00453dd5c2256e27cd4711e28c49b64bf4ec7f2
xixici <202388990@qq.com>
2019-03-16 12:29:01 +0800

    2019-03-16 12:29:00

---
+++ src/main/scala/com/xixici/P49.scala
@@ -0,0 +1,18 @@
+package com.xixici
+
+/**
+  * Created by xixici
+  * Date: 2019/3/16 
+  * Project Name: sword-offer-scala-sbt
+  * Project URL: https://github.com/xixici/sword-offer-scala
+  **/
+object P49 {
+  def StrToInt(str: String): Int = {
+    if (str.length == 0) {
+      return 0
+    }
+    val numList = Array('0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '-')
+
+    0
+  }
+}
