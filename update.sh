4863c9fe5d5eb367f919d92b5e95e6c7cbaee6b7
xixici <202388990@qq.com>
2019-03-16 12:36:33 +0800

    2019-03-16 12:36:33

--- update.sh
+++ update.sh
@@ -1,5 +1,10 @@
 git add -A
+echo "Add Finish"
 git commit -am "$(date "+%Y-%m-%d %H:%M:%S")"
+echo "Commit Finish"
 git pull
+echo "Pull Finish"
 git status
+echo "Status Finish"
 git push
+echo "push Finish"
