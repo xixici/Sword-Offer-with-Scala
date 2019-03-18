d98411915aae38f477245af712f22bdd22b343fb
xixici <202388990@qq.com>
2019-03-18 10:27:12 +0800

    xixici :taurus: push at 2019-03-18 10:27:11

--- update.sh
+++ update.sh
@@ -1,6 +1,10 @@
+emoji=( ♈️ :aries: ♉️ :taurus:	♊️ :gemini:	♋️ :cancer: ♌️ :leo:	♍️ :virgo:	♎️ :libra: ♏️ :scorpius:	♐️ :sagittarius: ♑️ :capricorn:
+♒️ :aquarius:	♓️ :pisces:	⛎ :ophiuchus: 🔯 :six_pointed_star: )
+
+
 git add -A
 echo "Add Finish"
-git commit -am "xixici :smile: push at $(date "+%Y-%m-%d %H:%M:%S")"
+git commit -am "xixici ${emoji[$(($RANDOM % ${#emoji[@]} + 1 ))]} push at $(date "+%Y-%m-%d %H:%M:%S")"
 echo "Commit Finish"
 git pull
 echo "Pull Finish"
