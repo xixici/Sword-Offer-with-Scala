1c5b55bc3f89bd5a8abc5f2076b699f697cd4f4c
xixici <202388990@qq.com>
2019-03-19 00:01:01 +0800

    ♉ xixici  push at 2019-03-19 00:01:00

---
+++ src/main/scala/com/xixici/P52.scala
@@ -0,0 +1,17 @@
+package com.xixici
+
+/**
+  * Created by xixici
+  * Date: 2019/3/18 
+  * Project Name: sword-offer-scala-sbt
+  * Project URL: https://github.com/xixici/sword-offer-scala
+  **/
+object P52 {
+  def matchStr(str: Array[Char], pattern: Array[Char]): Boolean = {
+    if (str == null && pattern == null) return true
+    if (pattern == null) return true
+    var flag = true
+    flag
+
+  }
+}
