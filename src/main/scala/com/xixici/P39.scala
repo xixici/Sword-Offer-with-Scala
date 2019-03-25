8e733054944a825b0457bca40234fabffb3b199d
xixici <202388990@qq.com>
2019-03-25 22:20:30 +0800

    ♐️ xixici  push at 2019-03-25 22:20:28

---
+++ src/main/scala/com/xixici/P39.scala
@@ -0,0 +1,18 @@
+package com.xixici
+
+import com.xixici.util.TreeNode
+
+/**
+  * Created by xixici
+  * Date: 2019/3/25 
+  * Project Name: sword-offer-scala-sbt
+  * Project URL: https://github.com/xixici/sword-offer-scala
+  **/
+object P39 {
+  def IsBalanced_Solution(root:TreeNode): Int = {
+
+
+    0
+  }
+
+}
---
+++ src/main/scala/com/xixici/P40.scala
@@ -0,0 +1,17 @@
+package com.xixici
+
+import com.xixici.util.TreeNode
+
+/**
+  * Created by xixici
+  * Date: 2019/3/25 
+  * Project Name: sword-offer-scala-sbt
+  * Project URL: https://github.com/xixici/sword-offer-scala
+  **/
+object P40 {
+  def IsBalanced_Solution(root:TreeNode): Int = {
+
+
+    0
+  }
+}
