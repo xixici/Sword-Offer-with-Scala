2b82e7103c523032b794eb559dfa081b16a7cc02
xixici <202388990@qq.com>
2019-04-25 22:35:52 +0800

    ♉ xixici  push at 2019-04-25 22:35:49

--- src/main/scala/com/xixici/P42.scala
+++ src/main/scala/com/xixici/P42.scala
@@ -1,5 +1,7 @@
 package com.xixici
 
+import scala.collection.mutable
+
 /**
   * Created by xixici
   * Date: 2019/3/30 
@@ -7,5 +9,21 @@ package com.xixici
   * Project URL: https://github.com/xixici/sword-offer-scala
   **/
 object P42 {
-
+  def FindNumbersWithSum(array: Array[Int], sum: Int): List[Int] = {
+    var i = 0
+    var j = array.length - 1
+    val list = mutable.Buffer[Int]()
+    while (i < j) {
+      if (array(i) + array(j) == sum) {
+        list.append(array(i))
+        list.append(array(j))
+        return list.toList
+      } else if (array(i) + array(j) > sum) {
+        j -= 1
+      } else {
+        i += 1
+      }
+    }
+    list.toList
+  }
 }
