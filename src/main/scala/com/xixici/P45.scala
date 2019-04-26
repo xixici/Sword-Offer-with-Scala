dd292286c03508b6f0b649c918fac32d8731c2f3
xixici <202388990@qq.com>
2019-04-26 21:34:41 +0800

    ♏ xixici  push at 2019-04-26 21:34:40

--- src/main/scala/com/xixici/P45.scala
+++ src/main/scala/com/xixici/P45.scala
@@ -7,5 +7,23 @@ package com.xixici
   * Project URL: https://github.com/xixici/sword-offer-scala
   **/
 object P45 {
+  def isContinuous(numbers: Array[Int]): Boolean = {
+    val numLen = numbers.length
+    if (numLen != 5) return false
+    var min = 14
+    var max = -1
+    var flag = 0
+    for (i <- 0 until numLen if numbers(i) != 0) {
+      val number = numbers(i)
+      if (number < 0 || number > 13) return false
+      //      if (number == 0) return
+      if (((flag >> number) & 1) == 1) return false
+      flag |= (1 << number)
+      if (number > max) max = number
+      if (number < min) min = number
+      if (max - min >= 5) return false
+    }
+    true
 
+  }
 }
