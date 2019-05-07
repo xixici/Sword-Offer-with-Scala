778c2b5e28b6a252b2c5fcf247a9291c51cb3313
xixici <202388990@qq.com>
2019-05-07 11:29:13 +0800

    ♑ xixici  push at 2019-05-07 11:29:12

---
+++ src/main/scala/com/xixici/util/TreeLinkNode.scala
@@ -0,0 +1,13 @@
+package com.xixici.util
+
+/**
+  * Created by xixici
+  * Date: 2019/5/7 
+  * Project Name: sword-offer-scala-sbt
+  * Project URL: https://github.com/xixici/sword-offer-scala
+  **/
+class TreeLinkNode(var data: Int) {
+  var left: TreeNode = _
+  var right: TreeNode = _
+  var next: TreeNode = _
+}
