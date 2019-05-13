73fb3cf0fb659846437661d667e079b5dad2f107
xixici <202388990@qq.com>
2019-05-13 19:07:55 +0800

    xixici  push at 2019-05-13 19:07:53

--- src/main/scala/com/xixici/P62.scala
+++ src/main/scala/com/xixici/P62.scala
@@ -1,5 +1,7 @@
 package com.xixici
 
+import com.xixici.util.TreeNode
+
 /**
   * Created by xixici
   * Date: 2019/5/9 
@@ -7,5 +9,20 @@ package com.xixici
   * Project URL: https://github.com/xixici/sword-offer-scala
   **/
 object P62 {
+  var index = 0
 
+  def KthNode(root: TreeNode, k: Int): TreeNode = {
+    if (root != null) {
+      var node = KthNode(root.left, k)
+      if (node != null)
+        return node
+      index += 1
+      if (index == k)
+        return root
+      node = KthNode(root.right, k)
+      if (node != null)
+        return node
+    }
+    null
+  }
 }
---
+++ src/main/scala/com/xixici/p63.scala
@@ -0,0 +1,11 @@
+package com.xixici
+
+/**
+  * Created by xixici
+  * Date: 2019/5/13 
+  * Project Name: sword-offer-scala-sbt
+  * Project URL: https://github.com/xixici/sword-offer-scala
+  **/
+object P63 {
+  
+}
